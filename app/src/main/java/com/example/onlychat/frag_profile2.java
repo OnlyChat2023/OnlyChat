e1dfb1016d86005e4e38daacfa160f31e2522f2a
PhongTran1804 <trannguyenphongg@gmail.com>
2023-03-19 17:54:22 +0700

    profile screen with all details

--- app/src/main/java/com/example/onlychat/frag_profile2.java
+++ app/src/main/java/com/example/onlychat/frag_profile2.java
@@ -1,5 +1,6 @@
 package com.example.onlychat;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +27,13 @@ public class frag_profile2 extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+//    MainActivity main;
+    Context context = null;
+    private CustomIconLabelAdapter adapter;
+
+    String[] contents = {"Facebook", "Instagram"};
+    Integer[] thumbnails = { R.drawable.ic_facebook,R.drawable.ic_instagram};
 
     public frag_profile2() {
         // Required empty public constructor
@@ -49,6 +60,14 @@ public class frag_profile2 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            context = getActivity(); // use this reference to invoke main callbacks
+//            main = (MainActivity) getActivity();
+        }
+        catch (IllegalStateException e) {
+            throw new IllegalStateException("MainActivity must implement callbacks");
+        }
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -58,7 +77,17 @@ public class frag_profile2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frag_profile2, container, false);
+        LinearLayout layout_profile2 = (LinearLayout) inflater.inflate(R.layout.fragment_frag_profile2, null);
+        ListView listView = (ListView) layout_profile2.findViewById(R.id.list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(context,
+                R.layout.info_item, contents);
+
+        adapter = new CustomIconLabelAdapter(context, R.layout.info_item, contents, thumbnails);
+        listView.setAdapter(adapter);
+        listView.setSelection(0);
+        listView.smoothScrollToPosition(0);
+
+        return layout_profile2;
     }
 }
