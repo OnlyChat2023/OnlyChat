0502d371e5ddeced267c0bb6c3e5322d56745031
NG-MinG <wgmin.it@outlook.com>
2023-03-21 20:33:06 +0700

    create register form

--- app/src/main/java/com/example/onlychat/MainActivity.java
+++ app/src/main/java/com/example/onlychat/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private final Boolean isLogin = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if (isLogin == false) {
+            setContentView(R.layout.register_form);
+        }
     }
 }
