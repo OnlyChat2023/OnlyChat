e0e927595b61e291be520fc429da74c3280a11a2
khangduy017 <khangduy017@gmail.com>
2023-03-30 12:33:37 +0700

    Friends Invite Ui

--- app/src/main/java/com/example/onlychat/Friends/Invite/Invite.java
+++ app/src/main/java/com/example/onlychat/Friends/Invite/Invite.java
@@ -7,15 +7,42 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
+import com.example.onlychat.Friends.AllFriends.CustomFriendItem;
 import com.example.onlychat.R;
 
 public class Invite extends Fragment {
+    ListView listInvites;
 
+    String names[] = {
+            "Anonymous","Anonymous Private","Anonymous Publish",
+            "Anonymous","Anonymous Private","Anonymous Publish",
+            "Anonymous","Anonymous Private","Anonymous Publish",
+            "Anonymous","Anonymous Private","Anonymous Publish",
+    };
+    Integer avatars[]={
+            R.drawable.global_chat_avatar1,R.drawable.global_chat_avatar,R.drawable.global_chat_avatar2,
+            R.drawable.global_chat_avatar1,R.drawable.global_chat_avatar,R.drawable.global_chat_avatar2,
+            R.drawable.global_chat_avatar1,R.drawable.global_chat_avatar,R.drawable.global_chat_avatar2,
+            R.drawable.global_chat_avatar1,R.drawable.global_chat_avatar,R.drawable.global_chat_avatar2
+    };
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.friends_fragment_invite, container, false);
+        LinearLayout invite = (LinearLayout) inflater.inflate(R.layout.friends_fragment_invite, null);
+
+        listInvites = (ListView) invite.findViewById(R.id.listInvites);
+
+        listInvites.setSelection(0);
+        listInvites.smoothScrollToPosition(0);
+        listInvites.setDivider(null);
+        listInvites.setDividerHeight(0);
+
+        CustomInviteItem customInviteItem=new CustomInviteItem(getActivity(),avatars,names);
+        listInvites.setAdapter(customInviteItem);
+
+        return invite;
     }
 }
