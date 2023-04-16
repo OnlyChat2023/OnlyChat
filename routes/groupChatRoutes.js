7eada14fc7e86bf3a9c2fe47e422379023e745e8
Huynh Tuan Duy <74585439+tuanduy18012002@users.noreply.github.com>
2023-04-16 17:05:55 +0700

    leave Group Chat

--- routes/groupChatRoutes.js
+++ routes/groupChatRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addGroup, getListGroupChat } from '../controllers/groupChatController.js';
+import { addGroup, getListGroupChat, leaveGroupChat } from '../controllers/groupChatController.js';
 
 const router = express.Router()
 
 router.post('/addGroup', addGroup);
 router.post('/getListGroupChat', getListGroupChat);
+router.post('/leaveGroupChat', leaveGroupChat);
 
 export default router
