794c206c2d2edecc12866fbef6ae00997282f68b
NG-MinG <wgmin.it@outlook.com>
2023-04-16 18:59:04 +0700

    fix model

--- models/botChatModel.js
+++ models/botChatModel.js
@@ -1,13 +1,11 @@
 import mongoose from 'mongoose';
 
 const messageSchema = new mongoose.Schema({
-  _id: mongoose.Schema.ObjectId,
   id_user: String,
   name: { type: String, default: '' },
   avatar: { type: String, default: '' },
   chats: [
     {
-      _id: mongoose.Schema.ObjectId,
       message: String,
       time: Date,
       user_id: String,
--- models/directChatModel.js
+++ models/directChatModel.js
@@ -1,12 +1,10 @@
 import mongoose from 'mongoose';
 
 const messageSchema = new mongoose.Schema({
-  _id: mongoose.Schema.ObjectId,
   avatar: { type: String, default: '' },
   name: { type: String, default: '' },
   chats: [
     {
-      _id: mongoose.Schema.ObjectId,
       message: String,
       time: Date,
       user_id: String,
