3dcfd99b61330dbcbceae275df2312078b138fd2
Huynh Tuan Duy <74585439+tuanduy18012002@users.noreply.github.com>
2023-04-18 20:13:15 +0700

    directMessage Option

--- controllers/directMessageController.js
+++ controllers/directMessageController.js
@@ -2,6 +2,14 @@ import directChat from "../models/directChatModel.js";
 import User from "../models/userModel.js";
 import catchAsync from "../utils/catchAsync.js";
 
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach((el) => {
+      if (allowedFields.includes(el)) newObj[el] = obj[el];
+    });
+    return newObj;
+  };
+
 const addDirectMessage = catchAsync(async (req, res, next) => {
     const user1 = await User.findOne({_id: req.body.id_1});
     const user2 = await User.findOne({_id: req.body.id_2});
@@ -49,4 +57,33 @@ const addDirectMessage = catchAsync(async (req, res, next) => {
       });
 });
 
-export {addDirectMessage}
+const changeOptions = catchAsync(async (req, res, next) => {
+    const filterBody = filterObj(req.body, 'user_id', 'notify', 'block');
+    filterBody.notify = (req.body.notify === 'true');
+    filterBody.block = (req.body.block === 'true');
+    
+    const updateDirectMessage = await directChat.updateOne({_id : req.body.dmc_id, "options.user_id" : req.body.user_id}, {$set : {"options.$": filterBody}});
+
+    res.status(200).json({ status: 'success', data: {}});
+});
+
+const getBlock = catchAsync(async (req, res, next) => {
+    const chat = await directChat.findOne({_id: req.body.dmc_id});
+    var block = undefined;
+    for (let i of chat.options){
+        if (i.user_id === req.body.user_id){
+            block = i.block;
+            break;
+        }
+    }
+
+    res.status(200).json({ status: 'success', data: block});
+});
+
+const changeNickname = catchAsync(async (req, res, next) => {
+    const updateNickname = await directChat.updateOne({_id : req.body.dmc_id, "members.user_id" : req.body.user_id}, {$set : {"members.$.nickname": req.body.nickname}});
+
+    res.status(200).json({ status: 'success', data: {}});
+});
+
+export {addDirectMessage, changeOptions, getBlock, changeNickname}
